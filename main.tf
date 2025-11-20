4b4ccd1b339ecc2b03ec20f76fb4315993d01736
dilipkumar975 <69560818+dilipkumar975@users.noreply.github.com>
2025-11-20 12:54:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
 resource "aws_lambda_function" "container_lambda" {
   function_name = "my-container-lambda"
   package_type  = "Image"
-  image_uri     = "<your-ecr-uri>:latest" # Replace with your actual ECR image URI
+  image_uri     = "539935451710.dkr.ecr.us-west-1.amazonaws.com/patient-service:latest" # Replace with your actual ECR image URI
   role          = aws_iam_role.lambda_exec_role.arn
   timeout       = 10
   memory_size   = 128
