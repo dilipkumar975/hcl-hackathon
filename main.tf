4220d9f26cf5bf673f84bb1e327c5a04578afe78
dilipkumar975 <69560818+dilipkumar975@users.noreply.github.com>
2025-11-20 12:57:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,11 @@ resource "aws_lambda_function" "container_lambda" {
   timeout       = 10
   memory_size   = 128
 }
+resource "aws_lambda_function" "container_lambda" {
+  function_name = "my-container-lambda"
+  package_type  = "Image"
+  image_uri     = "539935451710.dkr.ecr.us-west-1.amazonaws.com/appointment-service:latest" # Replace with your actual ECR image URI
+  role          = aws_iam_role.lambda_exec_role.arn
+  timeout       = 10
+  memory_size   = 128
+}
